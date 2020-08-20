b4ff9f21de7ea8cc9447c7b5c446182c7ec39980
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-08-20 13:08:43 +0200

    Created blog and request.rest file

--- app.js
+++ app.js
@@ -1,53 +1,75 @@
 import dotenv from "dotenv";
 dotenv.config();
 import http from "http";
+import request from "request";
 import path from "path";
 import morgan from "morgan";
 import express from "express";
 import mongoose from "mongoose";
+import Blog from "./models/blog.js";
+import fs from "fs";
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 //connect to mongodb
 const dbURI =
   "mongodb+srv://iran212:iran212@cluster0.n4lci.mongodb.net/iran212?retryWrites=true&w=majority";
+
 mongoose
   .connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then((result) => console.log("Connected to mongodb"))
+  .then((result) => {
+    app.listen(3333);
+    console.log("Connected to mongodb");
+  })
   .catch((err) => console.log(err));
-app.set("view engine", "ejs");
+
 const __dirname = path.resolve(path.dirname(""));
 app.use(express.json());
-app.use(express.static("public"));
-const posts = [
-  {
-    username: "Ali",
-    title: "post 1",
-  },
-  {
-    username: "Benjamin",
-    title: "post 2",
-  },
-  {
-    username: "Billy",
-    title: "post 3",
-  },
-];
+
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+let myBlogs = [];
+
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "New blog 3",
+    snippet: "About new blog 3",
+    body: "More about my new blog 3",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+      myBlogs.push(result);
+      var words = JSON.stringify(myBlogs, null, 2);
+      fs.appendFileSync("data.json", words);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/single-blog", (req, res) => {
+  Blog.findById("5f3d1d5d3792747758a20496")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+//////////////////////////////
+
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
@@ -55,8 +77,63 @@ app.get("/about", (req, res) => {
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
 });
+//blogs route
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", {
+        title: "All blogs",
+        blogs: result,
+        weather: null,
+        error: null,
+      });
+      //console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.post("/blogs", (req, res) => {
+  console.log(req.body);
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+      res.redirect("/blogs");
+      myBlogs.push(result);
+      fs.writeFileSync("./data.json", myBlogs.join(","), "utf-8");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.put("/blogs/:id", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var snippet = req.body.snippet;
+  var body = req.body.body;
+  Blog.update({ id, title, snippet, body }, (id) => {
+    res.redirect("/blogs/" + id);
+  });
+});
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id).then((result) => {
+    res.json({ redirect: "/blogs" });
+  });
+});
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
-
-app.listen(3333);
